4178d202a4fd15d6aca7cce9a734f4d2bd7e12c4
iLexiconn <ilexiconn@gmail.com>
2015-11-22 21:33:43 +0100

    Updated version number

--- src/main/java/net/ilexiconn/chatalert/ChatAlert.java
+++ src/main/java/net/ilexiconn/chatalert/ChatAlert.java
@@ -15,7 +15,7 @@ public class ChatAlert {
     public static ServerProxy proxy;
     public static LoggerHelper logger = new LoggerHelper("ChatAlert");
 
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
